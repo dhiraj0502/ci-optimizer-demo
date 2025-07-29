4eea5c5a25474ff698af2a6f090a66d531959ad0
Dhiraj Kuslekar <dhirajkuslekar45@gmail.com>
2025-07-29 11:28:05 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const indexRoute = require("./routes/index");
+
+app.use("/", indexRoute);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
